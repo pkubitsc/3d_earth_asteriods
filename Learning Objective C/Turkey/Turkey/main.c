11afece4f874c9e800343421c250ac472660694f
Paul Kubitschek <Paul1Kubi@Pauls-MacBook-Pro-5.local>
2015-01-04 22:07:48 -0600

    new file:   main.c
    	first file

---
+++ Learning Objective C/Turkey/Turkey/main.c
@@ -0,0 +1,42 @@
+/
+//  main.c
+//  Turkey
+//
+//  Created by Paul Kubitschek on 1/3/15.
+//  Copyright (c) 2015 Paul Kubitschek. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    //Declare the variable called 'weight' of type floate
+    float weight;
+    
+    //put a number in that variable
+    weight = 14.2;
+    
+    // Log it to the user
+    printf("The turkey weights %f.\n", weight);
+    
+    // declare another variable of type float
+    float cookingTime;
+ 
+
+
+
+	// ;alsdjf;a lskdjf;lasjkdf; lajsd;flkja s;dlfkja;sldkfja;slkdjf ;lkasj dl;fkj 
+
+
+   
+    //Calculate the cooking time and store it in the variable
+    // In this case, '*' means "multipled by'
+    cookingTime = 15.0 + 15.0 * weight;
+    
+    //Log that to the user
+    printf("Cook it for %f minutes.\n", cookingTime);
+    
+    //End this function and indicate success
+    
+    return 0;
+}
